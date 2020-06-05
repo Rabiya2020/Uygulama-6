7fd141e7626ea3b1786cbc5f3b89e2a9a79fab59
Rabiya2020 <65968900+Rabiya2020@users.noreply.github.com>
2020-06-05 18:37:32 +0300

    Add files via upload

---
+++ Uygulama6/src/uygulama6/Uygulama6.java
@@ -0,0 +1,21 @@
+
+package uygulama6;
+
+
+public class Uygulama6 {
+
+    
+    public static void main(String[] args) 
+    {
+        cıkar();
+        //Ana metodumuz olan main metodumuzun içerisinde işlemimiizin sonucunu ekrana yazdırıyor.
+    }
+    
+    static void cıkar()
+    //Geriye değer döndürmeyen ve parametresiz cıkar adında metod oluşturduk.
+    {
+        System.out.println("Sonuç = "+ (32-5));
+        //Bu metod 32 den 5 i çıkararak ekrana yazdırıyor.
+    }
+    
+}
